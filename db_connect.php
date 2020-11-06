0d0852b7bf0bae5939d6992839920543d61c2eef
sruthireact <73086192+sruthireact@users.noreply.github.com>
2020-11-07 03:11:43 +0530

    Local to Github

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+/* Database connection start */
+//$servername = "mysql://bb25f8f33875df:fde0a2c2@us-cdbr-east-02.cleardb.com/heroku_c8f9676cdad8a4d?reconnect=true";
+$servername = "us-cdbr-east-02.cleardb.com";
+$username = "bb25f8f33875df";
+$password = "	fde0a2c2";
+$dbname = "heroku_c8f9676cdad8a4d";
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+?>

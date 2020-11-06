291e6200062cb4a88c1085beccff0f7ba120b277
sruthireact <73086192+sruthireact@users.noreply.github.com>
2020-11-07 03:16:15 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@
 //$servername = "mysql://bb25f8f33875df:fde0a2c2@us-cdbr-east-02.cleardb.com/heroku_c8f9676cdad8a4d?reconnect=true";
 $servername = "us-cdbr-east-02.cleardb.com";
 $username = "bb25f8f33875df";
-$password = "	fde0a2c2";
+$password = "fde0a2c2";
 $dbname = "heroku_c8f9676cdad8a4d";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
